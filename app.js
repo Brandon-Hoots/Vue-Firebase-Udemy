7d1caa3989efff23a2170f3e46b698cd677cb70c
Brandon Hoots <Bhoots@students.kennesaw.edu>
2020-11-04 18:00:02 -0500

    First part of the tutorial

--- app.js
+++ app.js
@@ -3,13 +3,58 @@ new Vue({
     data: {
         name: 'Teams',
         url: "http://www.youtube.com",
-        classes: ['one', 'two']
+        classes: ['one', 'two'],
+        wage: 10,
+        coords: {
+            x: 0,
+            y: 0
+        },
+        showName: true,
+        showAge: true,
+        items: ['Mushrom', 'Green Shells', 'Red Shells', 'Banana', 'Star'],
+        ninjas: [
+            {name: 'Crystal', age: 25, belt: 'Black'},
+            {name: 'Ryu', age: 30, belt: 'Brown'},
+            {name: 'Ken', age: 35, belt: 'Orange'}
+        ]
     },
     methods: {
         greet(time){
             
             return `Hello there and good ${time}, ${this.name} `
+        },
+
+        changeWage(amount){
+            this.wage += amount
+        },
+
+        logEvent(e){
+            console.log(e)
+        },
+
+        logCoords(e){
+            this.coords.x = e.offsetX
+            this.coords.y = e.offsetY
+        },
+
+         updateName(e){
+            //console.log(e.target.value)
+            this.name = e.target.value
+         },
+
+         logMessage(){
+             console.log("Hello World")
+         },
+
+         toggleName(){
+             this.showName = !this.showName
+         },
+
+         toggleAge(){
+            this.showAge = !this.showAge
         }
+
     }
      
 })
+
